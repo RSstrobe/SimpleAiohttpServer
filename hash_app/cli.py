82ab91065ece5accf7f6f12ee2a8ba8cbef22fe1
Mosyagin Grigorii <94318830+RSstrobe@users.noreply.github.com>
2024-05-06 02:10:20 +0300

    Update cli.py

--- hash_app/cli.py
+++ hash_app/cli.py
@@ -8,7 +8,7 @@ from src.main import run_server
 @click.option('--host', prompt='Application host',
               help='Application host.', type=str, default=settings.app_host)
 @click.option('--port', prompt='Application port',
-              help='Application port.', type=int, default=settings.app_post)
+              help='Application port.', type=int, default=settings.app_port)
 def run(host: str, port: int):
     """Run the server"""
     run_server(host, port)
